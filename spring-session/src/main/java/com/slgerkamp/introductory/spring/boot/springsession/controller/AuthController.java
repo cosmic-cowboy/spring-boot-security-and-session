501b858a0a5e8d986f52a88c1ae0ab9338fc4a1e
hiroo takaku <slgerkamp@gmail.com>
2015-03-15 17:23:19 +0900

    ログアウト処理を追加

--- spring-session/src/main/java/com/slgerkamp/introductory/spring/boot/springsession/controller/AuthController.java
+++ spring-session/src/main/java/com/slgerkamp/introductory/spring/boot/springsession/controller/AuthController.java
@@ -2,7 +2,11 @@ package com.slgerkamp.introductory.spring.boot.springsession.controller;
 
 import java.security.Principal;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,5 +20,15 @@ public class AuthController {
     String check(Principal principal) {
         return principal.getName();
     }
-    
+
+	/**
+	 * ログアウトをする。
+	 * @param session
+	 */
+	@RequestMapping("/logout")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void logout(HttpSession session){
+		// セッションを破棄する
+		session.invalidate();
+	}
 }
